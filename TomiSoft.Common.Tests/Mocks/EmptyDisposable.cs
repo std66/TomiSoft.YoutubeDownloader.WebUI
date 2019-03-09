2a6418a8b14cc74839fadb25e9e7968f61f7a9e0
std66 <sinkutamas@gmail.com>
2019-03-09 21:41:06 +0100

    Cache system implemented.

---
+++ TomiSoft.Common.Tests/Mocks/EmptyDisposable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomiSoft.Common.Tests.Mocks {
+    class EmptyDisposable : IDisposable {
+        public void Dispose() {
+            
+        }
+    }
+}
---
+++ TomiSoft.Common/SystemClock/ITimerFactory.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TomiSoft.Common.SystemClock {
+    public interface ITimerFactory {
+        ITimer CreateTimer(int intervalInMilliseconds);
+        IDisposable ScheduleExecution(int intervalInMilliseconds, bool runOnce, Action action);
+    }
+}

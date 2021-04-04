b6c84d63f2103f55d7465d088dde865f06099e0f
std66 <sinkutamas@gmail.com>
2021-04-04 16:57:52 +0200

    Add /Home/Version API

--- TomiSoft.YouTubeDownloader.WebUI/Controllers/HomeController.cs
+++ TomiSoft.YouTubeDownloader.WebUI/Controllers/HomeController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using TomiSoft.YoutubeDownloader;
 using TomiSoft.YouTubeDownloader.WebUI.Models;
 
 namespace TomiSoft.YouTubeDownloader.WebUI.Controllers {
     public class HomeController : Controller {
+        private readonly IMediaDownloader dl;
+
+        public HomeController(IMediaDownloader dl) {
+            this.dl = dl;
+        }
+
         public IActionResult Index() {
             return View();
+        }
+
+        public async Task<IActionResult> VersionAsync() {
+            string youtubeDlVersion = null;
+            try {
+                 await Task.Run(() => youtubeDlVersion = dl.GetVersion());
+            }
+            catch {}
+            
+            return base.Json(new AppVersionInfo(youtubeDlVersion));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

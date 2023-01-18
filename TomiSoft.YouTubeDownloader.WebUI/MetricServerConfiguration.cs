dcf76a2a8bc089ffaf74c2d7f3e01c112924edfb
Sinku Tamás <sinkutamas@gmail.com>
2023-01-18 16:14:33 +0100

    Add metrics collection + NLog configmap support

---
+++ TomiSoft.YouTubeDownloader.WebUI/MetricServerConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace TomiSoft.YouTubeDownloader.WebUI
+{
+    public class MetricServerConfiguration
+    {
+        public bool Enabled { get; set; }
+        public ushort Port { get; set; }
+        public string Uri { get; set; }
+    }
+}
--- TomiSoft.YouTubeDownloader.WebUI/Startup.cs
+++ TomiSoft.YouTubeDownloader.WebUI/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Prometheus;
 using TomiSoft.Common.FileManagement;
 using TomiSoft.YoutubeDownloader;
 using TomiSoft.YoutubeDownloader.BusinessLogic;
@@ -16,33 +18,49 @@ using TomiSoft.YouTubeDownloader.WebUI.Hubs;
 
 namespace TomiSoft.YouTubeDownloader.WebUI
 {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
+
+            MetricServerConfiguration config = new MetricServerConfiguration();
+            configuration.GetSection("Metrics").Bind(config);
+            MetricsConfiguration = config;
+
+            YoutubeConfiguration ytconfig = new YoutubeConfiguration();
+            Configuration.GetSection("YoutubeConfiguration").Bind(config);
+            YoutubeConfiguration = ytconfig;
         }
 
         public IConfiguration Configuration { get; }
 
-        private YoutubeConfiguration YoutubeConfiguration {
-            get {
-                YoutubeConfiguration config = new YoutubeConfiguration();
-                Configuration.GetSection("YoutubeConfiguration").Bind(config);
+        private YoutubeConfiguration YoutubeConfiguration { get; }
 
-                return config;
-            }
-        }
-        
+        private MetricServerConfiguration MetricsConfiguration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.Configure<CookiePolicyOptions>(options => {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            if (MetricsConfiguration.Enabled)
+            {
+                services.AddMetricServer(options =>
+                {
+                    options.Url = MetricsConfiguration.Uri;
+                    options.Port = MetricsConfiguration.Port;
+                });
+            }
+
             services
                 .AddYoutubeDownloaderCore();
-            
+
             services
                 .AddSingleton<IYoutubeDlConfiguration>(YoutubeConfiguration)
                 .AddSingleton<IDownloaderServiceConfiguration>(YoutubeConfiguration)
@@ -62,27 +80,42 @@ namespace TomiSoft.YouTubeDownloader.WebUI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> log)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
             }
-            else {
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app
+                .UseHttpMetrics()
+                .UseStaticFiles()
+                .UseCookiePolicy()
+                .UseRouting()
+                .UseEndpoints(builder =>
+                {
+                    if (MetricsConfiguration.Enabled)
+                    {
+                        builder.MapMetrics();
+                        log.LogInformation("Metric collection is enabled on port '{Port}' and path '{Uri}'", MetricsConfiguration.Port, MetricsConfiguration.Uri);
+                    }
+                    else
+                    {
+                        log.LogInformation("Metric collection is disabled in configuration.");
+                    }
 
-            app.UseRouting();
-            app.UseEndpoints(builder => {
-                builder.MapHub<DownloadHub>("/downloadHub");
-            });
-
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                    builder.MapHub<DownloadHub>("/downloadHub");
+                })
+                .UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                });
         }
     }
 }
